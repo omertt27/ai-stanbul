6dd77f016d16a8bb8fe9a3c9a2e6491621df9a0c
omerTT <omertahtoko@gmail.com>
2025-08-20 18:58:12 +0300

    Update restaurants.py

---
+++ backend/routes/restaurants.py
@@ -0,0 +1,15 @@
+import requests
+
+GOOGLE_API_KEY = "R3g!on9$Plx"
+BASE_URL = "https://maps.googleapis.com/maps/api/place/"
+
+def search_restaurants(location, radius=1000, type="restaurant"):
+    url = f"{BASE_URL}nearbysearch/json"
+    params = {
+        "key": GOOGLE_API_KEY,
+        "location": location,  
+        "radius": radius,
+        "type": type
+    }
+    response = requests.get(url, params=params)
+    return response.json()
