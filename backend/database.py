68724a211697f42079286b0d1f321d304b1ea861
omerTT <omertahtoko@gmail.com>
2025-08-20 18:46:21 +0300

    sets

--- backend/database.py
+++ backend/database.py
@@ -1,18 +1,13 @@
-# backend/database.py
-import os
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-from models import Base
 
-load_dotenv()
+SQLALCHEMY_DATABASE_URL = "sqlite:///./ai_stanbul"
 
-DB_URL = os.getenv("DATABASE_URL")
-if not DB_URL:
-    raise ValueError("DATABASE_URL not found! Check your .env file.")
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}  # needed for SQLite
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-engine = create_engine(DB_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
 
-# Create tables
-Base.metadata.create_all(engine)
