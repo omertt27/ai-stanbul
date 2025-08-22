a728f579711bbd50174824cfeb37c04ccd1ace5b
omerTT <omertahtoko@gmail.com>
2025-08-22 15:20:09 +0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -8,14 +8,13 @@ class User(Base):
     name = Column(String(100), nullable=False)
     email = Column(String(100), unique=True, nullable=False)
 
-class Place(Base):
+class places(Base):
     __tablename__ = "places"
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(255), nullable=False)
     category = Column(String(50))
-    lat = Column(Float)
-    lng = Column(Float)
-
+    district = Column(String(50))
+    
 class Museum(Base):
     __tablename__ = "museums"
     id = Column(Integer, primary_key=True, index=True)
