51566b1ae77b7b6becf68478d8d436ac7ce38eb3
omerTT <omertahtoko@gmail.com>
2025-08-22 15:17:25 +0300

    sql

--- backend/routes/museums.py
+++ backend/routes/museums.py
@@ -1,13 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
 
-class Museum(Base):
-    __tablename__ = "museums"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, nullable=False)
-    location = Column(String, nullable=False)
-    
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import SessionLocal
