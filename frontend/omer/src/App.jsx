4f52e555cfbecde73e2586f20025bd4bac92c340
omerTT <omertahtoko@gmail.com>
2025-08-24 14:10:10 +0300

    Update App.jsx

--- frontend/omer/src/App.jsx
+++ frontend/omer/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-<h1>Welcome to the istanbul!</h1>
 import './App.css'
 
 function App() {
@@ -7,14 +6,17 @@ function App() {
 
   return (
     <>
+      <h1>Welcome to Istanbul!</h1>
+
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="/react.svg" className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
